a987b82113d1f86ff2d3e5df839898c597ba8da9
Lyte Onyema <lyte.onyema@gmail.com>
2022-09-06 20:14:18 +0100

    db bug fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->string('username', 191)->unique();
+            $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
